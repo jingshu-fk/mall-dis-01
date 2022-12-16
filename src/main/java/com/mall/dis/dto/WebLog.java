e269adeb3ac489bbd461d8fe1c58f76eb4a1a854
shujingping <sjp19941119@163.com>
2022-12-16 16:30:13 +0800

    新增aop记录接口访问日志

---
+++ src/main/java/com/mall/dis/dto/WebLog.java
@@ -0,0 +1,70 @@
+
+package com.mall.dis.dto;
+
+import lombok.Data;
+
+/**
+ * WebLog
+ * @description 接口日志
+ * @author shujingping
+ * @date 2022/12/16 10:51 上午
+ * @version 1.0
+ */
+@Data
+public class WebLog {
+    /**
+     * 操作描述
+     */
+    private String description;
+
+    /**
+     * 操作用户
+     */
+    private String username;
+
+    /**
+     * 操作时间
+     */
+    private Long startTime;
+
+    /**
+     * 消耗时间
+     */
+    private Integer spendTime;
+
+    /**
+     * 根路径
+     */
+    private String basePath;
+
+    /**
+     * URI
+     */
+    private String uri;
+
+    /**
+     * URL
+     */
+    private String url;
+
+    /**
+     * 请求类型
+     */
+    private String method;
+
+    /**
+     * IP地址
+     */
+    private String ip;
+
+    /**
+     * 请求参数
+     */
+    private Object parameter;
+
+    /**
+     * 请求返回的结果
+     */
+    private Object result;
+}
+ 
